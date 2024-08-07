eb13b911bda3ae84142dc96b2ca54f32891f7a15
liyon <svliyon@gmail.com>
2024-08-07 13:26:59 +1000

    1.10-refactoring

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,20 +14,52 @@ const Button = (props) => {
   )
 }
 
-const Count = (props) => {
-  if(props.text === "positive")
-  return (
+const Buttons = (props)=>{
+  return(
     <>
-      <p>{props.text} {props.count} %</p>
+      <Button text={props.text.good} onClick={props.handlers.handleGood}></Button>
+      <Button text={props.text.neutral} onClick={props.handlers.handleNeutral}></Button>
+      <Button text={props.text.bad} onClick={props.handlers.handleBad}></Button>    
     </>
   )
-  else{
+}
+
+const StatisticLine = (props) => {
+  if (props.text === "positive")
+    return (
+      <>
+        <p>{props.text} {props.count} %</p>
+      </>
+    )
+  else {
     return (
       <>
         <p>{props.text} {props.count}</p>
       </>
-    )    
+    )
   }
+}
+
+const Statistics = (props) => {
+  if(props.statistics.all === 0){
+    return(
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  else{
+  return (
+    <div>
+     <StatisticLine text={props.text.good} count={props.statistics.good}></StatisticLine>
+      <StatisticLine text={props.text.neutral} count={props.statistics.neutral}></StatisticLine >
+      <StatisticLine text={props.text.bad} count={props.statistics.bad}></StatisticLine>
+      <StatisticLine text={props.text.all} count={props.statistics.all}></StatisticLine >
+      <StatisticLine text={props.text.average} count={props.statistics.average}></StatisticLine >
+      <StatisticLine text={props.text.positive} count={props.statistics.positive}></StatisticLine> 
+    </div>
+  )
+}
 }
 
 const App = () => {
@@ -35,9 +67,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all,setAll] = useState(0)
-  const [average,setAverage] = useState(0)
-  const [positive,setPositive] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const title = {
     feedback: "give feedback",
@@ -48,63 +80,71 @@ const App = () => {
     good: "good",
     neutral: "neutral",
     bad: "bad",
-    all : "all",
-    average : "average",
-    positive : "positive"
+    all: "all",
+    average: "average",
+    positive: "positive"
   }
+
+  const statistics = {
+    good: good,
+    neutral: neutral,
+    bad: bad,
+    all: all,
+    average: average,
+    positive: positive
+  }  
 
   const handleGood = () => {
     const updatedGood = good + 1
     setGood(updatedGood)
-    updateAll()
-    updatePositive(updatedGood,all+1)
-    updateAverage(updatedGood,neutral,bad)
+    updateAll(updatedGood, neutral, bad)
+    updatePositive(updatedGood, neutral, bad)
+    updateAverage(updatedGood, neutral, bad)
   }
 
   const handleNeutral = () => {
     const updatedNuetral = neutral + 1
     setNeutral(updatedNuetral)
-    updateAll()   
-    updatePositive(good,all+1)
-    updateAverage(good,neutral,bad)
+    updateAll(good, updatedNuetral, bad)
+    updatePositive(good, updatedNuetral, bad)
+    updateAverage(good, updatedNuetral, bad)
   }
 
   const handleBad = () => {
     const updatedBad = bad + 1
-    setBad(updatedBad) 
-    updateAll()
-    updatePositive(good,all+1)
-    updateAverage(good,neutral,updatedBad)
+    setBad(updatedBad)
+    updateAll(good, neutral, updatedBad)
+    updatePositive(good, neutral, updatedBad)
+    updateAverage(good, neutral, updatedBad)
   }
 
-  const updateAll = ()=>{
-    const updatedAll = all + 1
-    setAll(updatedAll)   
+  const eventHanlders = {
+    handleGood : handleGood,
+    handleNeutral : handleNeutral,
+    handleBad : handleBad
   }
 
-  const updatePositive = (good,all)=>{
-    const updatedPositive = ( good / all ) * 100
+  const updateAll = (good, neutral, bad) => {
+    const updatedAll = good + neutral + bad
+    setAll(updatedAll)
+  }
+
+  const updatePositive = (good, neutral, bad) => {
+    const updatedPositive = (good / (good + neutral + bad)) * 100
     setPositive(updatedPositive)
   }
 
-  const updateAverage = (good,neutral,bad)=>{
-    const updatedAverage = ( ( good * 1 ) + ( neutral * 0 ) + ( bad * -1 ) ) / ( good + neutral + bad )
+  const updateAverage = (good, neutral, bad) => {
+    const updatedAverage = ((good * 1) + (neutral * 0) + (bad * -1)) / (good + neutral + bad)
     setAverage(updatedAverage)
   }
 
   return (
     <div>
       <Header title={title.feedback}></Header>
-      <Button text={text.good} onClick={handleGood}></Button>
-      <Button text={text.neutral} onClick={handleNeutral}></Button>
-      <Button text={text.bad} onClick={handleBad}></Button>
+      <Buttons text={text} handlers={eventHanlders}></Buttons>
       <Header title={title.statistics}></Header>
-      <Count text={text.good} count={good}></Count>
-      <Count text={text.neutral} count={neutral}></Count>
-      <Count text={text.bad} count={bad}></Count>
-      <Count text={text.all} count={all}></Count>
-      <Count text={text.average} count={average}></Count>
-      <Count text={text.positive} count={positive}></Count>
+      <Statistics text={text} statistics={statistics}></Statistics>
     </div>
   )
 }
