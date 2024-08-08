850a445716557c9593a59861663a19e6e6c59f4f
liyon <svliyon@gmail.com>
2024-08-08 12:38:26 +1000

    2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Person = ({person }) => {
+const Person = ({ person }) => {
   return (
     <p>{person.name}</p>
   )
@@ -9,24 +9,70 @@ const Person = ({person }) => {
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
 
   //on form submit
   const addPerson = (event) => {
     event.preventDefault()
-    const personObject = {
+    const newPerson = {
       name: newName
     }
-    //add new person object
-    setPersons(persons.concat(personObject))
-    setNewName('')  
+
+    //check if the person exists in the array of objects
+    let personAdded = false
+    //loop through and check if the person is already added
+    persons.forEach(person => {
+      if (areTheseObjectsEqual(newPerson, person)) {
+        personAdded = true
+        return
+      }
+    }
+    )
+    if (personAdded) {
+      alert(`${newName} is already added to phonebook`)
+    }
+    else {
+      //add new person object
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+    }
   }
+
+
 
   //reads the name from the input field and sets
   const handlePersonChange = (event) => {
     setNewName(event.target.value)
-  }  
+  }
+
+  function areTheseObjectsEqual(first, second) {
+    const al = Object.getOwnPropertyNames(first);
+    const bl = Object.getOwnPropertyNames(second);
+
+    // Check if the two list of keys are the same
+    // length. If they are not, we know the objects
+    // are not equal.
+    if (al.length !== bl.length) return false;
+
+    // Check that all keys from both objects match
+    // are present on both objects.
+    const hasAllKeys = al.every((value) => !!bl.find((v) => v === value));
+
+    // If not all the keys match, we know the
+    // objects are not equal.
+    if (!hasAllKeys) return false;
+
+    // We can now check that the value of each
+    // key matches its corresponding key in the
+    // other object.
+    for (const key of al) if (first[key] !== second[key]) return false;
+
+    // If the object hasn't return yet, at this
+    // point we know that the objects are the
+    // same
+    return true;
+  }
 
   return (
     <div>
@@ -41,9 +87,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-          {persons.map(person => 
-            <Person key={person.name} person={person} />
-          )}
+        {persons.map(person =>
+          <Person key={person.name} person={person} />
+        )}
       </div>
     </div>
   )
